1097fbae79bdfd671ffb317c9142bd01e34123cd
Christian Gebbe <>
2024-09-17 17:08:13 +0200

    fix issue: when opening domain, role has no permission for sagemaker:CreateApp

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -1,22 +1,50 @@
 # sagemaker domain
 # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sagemaker_domain
+resource "aws_iam_role" "sagemaker_execution_role" {
+  name = "my-sagemaker-role2"
 
-data "aws_iam_policy_document" "example" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["sagemaker.amazonaws.com"]
-    }
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          Service = "sagemaker.amazonaws.com"
+        },
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role" "my-sagemaker-role" {
-  name               = "my-sagemaker-role"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.example.json
+resource "aws_iam_policy" "sagemaker_policy" {
+  name        = "SageMakerFullAccessPolicy"
+  description = "Policy to allow all SageMaker related actions"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "sagemaker:*",  # Allows all SageMaker actions
+          "s3:*",         # Optional: Access to S3 for input/output
+          "ecr:*",        # Optional: Access to Elastic Container Registry (ECR)
+          "cloudwatch:*", # Optional: CloudWatch for logs/metrics
+          "logs:*",
+          "iam:PassRole"  # Allows SageMaker to pass roles
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
+
+resource "aws_iam_role_policy_attachment" "attach_sagemaker_policy" {
+  role       = aws_iam_role.sagemaker_execution_role.name
+  policy_arn = aws_iam_policy.sagemaker_policy.arn
+}
+
 
 resource "aws_sagemaker_domain" "example" {
   domain_name = "dev"
@@ -25,7 +53,7 @@ resource "aws_sagemaker_domain" "example" {
   subnet_ids  = module.vpc.private_subnets
 
   default_user_settings {
-    execution_role = aws_iam_role.my-sagemaker-role.arn
+    execution_role = aws_iam_role.sagemaker_execution_role.arn
   }
 }
 
